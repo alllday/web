e21e3e57d7be919777e48bd6fd2aa4c80a0514a8
alllday <124110632+alllday@users.noreply.github.com>
2023-02-02 19:57:36 +0900

    Add files via upload

---
+++ day4/out.js
@@ -0,0 +1,11 @@
+function Laundry() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물과 비누를 넣는다.")
+    console.log("3. 비비고 헹군다.")
+    console.log("4. 탈수후 넌다.")
+}
+function Clean(num) {
+    for(i=0;i<num;i++){
+        console.log(i + "번 청소")
+    }
+}
---
+++ day4/out2.js
@@ -0,0 +1,10 @@
+function Trip() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물과 비누를 넣는다.")
+    console.log("3. 비비고 헹군다.")
+    console.log("4. 탈수후 넌다.")
+}
+function Rest() {
+    console.log("1. 청소키를 킨다")
+    console.log("2. 청소기를 돌린다.")
+}
